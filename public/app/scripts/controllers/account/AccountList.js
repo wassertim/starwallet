e5346158530f2204aa1ee31e62005773664ec040
wassertim <tim@digitalband.ru>
2014-03-26 20:13:39 +0400

    refactoring

--- public/app/scripts/controllers/account/AccountList.js
+++ public/app/scripts/controllers/account/AccountList.js
@@ -3,14 +3,17 @@
   function AccountController($scope, accountService, $stateParams, $state) {
     $scope.vm = this;
     $scope.href = $state.href;
-    this.userId = $stateParams['userId'];
-    accountService.list(this.userId).then(function(list){
-      $scope.list = list;
-    });
+    this.accountService = accountService;
+    this.list($stateParams['userId']);
   }
 
   AccountController.prototype = {
-
+    list: function(userId){
+      var that = this;
+      this.accountService.list(userId).then(function(list){
+        that.list = list;
+      });
+    }
   };
   app.controller('AccountController', AccountController);
   return AccountController;
