a2f9ae9c1afff5c7e2090de4e060400b80b77272
wassertim <tim@digitalband.ru>
2014-03-27 12:54:50 +0400

    missing files

--- public/app/scripts/services/fake/PromiseWrapper.js
+++ public/app/scripts/services/fake/PromiseWrapper.js
@@ -3,10 +3,13 @@
   function PromiseWrapper($q) {
     this.$q = $q;
   }
-  PromiseWrapper.prototype.wrap = function (fn) {
-    var deferred = this.$q.defer();
-    deferred.resolve(fn());
-    return deferred.promise;
+
+  PromiseWrapper.prototype = {
+    wrap: function (fn) {
+      var deferred = this.$q.defer();
+      deferred.resolve(fn());
+      return deferred.promise;
+    }
   };
 
   app.service('PromiseWrapper', PromiseWrapper);
