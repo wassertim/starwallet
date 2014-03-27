f8af8d0f6b90b4ab572f8e210c67a00293f928f5
wassertim <tim@digitalband.ru>
2014-03-27 17:53:12 +0400

    accountId parameter fixed

--- public/app/scripts/controllers/account/AccountDisplay.js
+++ public/app/scripts/controllers/account/AccountDisplay.js
@@ -3,16 +3,16 @@
   function AccountDisplayController($scope, $stateParams, accountService, $state) {
     $scope.vm = this;
     this.accountService = accountService;
-    this.getAccountInfo($stateParams['account']);
+    this.getAccountInfo($stateParams['accountId']);
     $scope.href = function(state, params){
       return $state.href(state, angular.extend($stateParams, params));
     };
   }
 
   AccountDisplayController.prototype = {
-    getAccountInfo: function(account) {
+    getAccountInfo: function(accountId) {
       var that = this;
-      this.accountService.getAccountInfo(account).then(function(accountInfo) {
+      this.accountService.getAccountInfo(accountId).then(function(accountInfo) {
         that.accountInfo = accountInfo;
       });
     }
