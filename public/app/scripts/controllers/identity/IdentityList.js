58bef7c6cca0cbf4a37e6184d4579ebd0fc8e4d8
wassertim <tim@digitalband.ru>
2014-03-31 16:18:21 +0400

    coupons count for all the identities

--- public/app/scripts/controllers/identity/IdentityList.js
+++ public/app/scripts/controllers/identity/IdentityList.js
@@ -1,11 +1,13 @@
 (function (app) {
-  IdentityListController.$inject = ['$scope', 'IdentityService', '$stateParams', '$state'];
-  function IdentityListController($scope, identityService, $stateParams, $state) {
+  IdentityListController.$inject = ['$scope', 'IdentityService', '$stateParams', '$state', 'AccountService'];
+  function IdentityListController($scope, identityService, $stateParams, $state, accountService) {
     $scope.vm = this;
+
     $scope.href = function(state, params){
       return $state.href(state, angular.extend($stateParams, params));
     };
     this.identityService = identityService;
+    this.accountService = accountService;
     this.list($stateParams['userId']);
   }
 
@@ -14,6 +16,14 @@
       var that = this;
       this.identityService.list(userId).then(function(list){
         that.list = list;
+        return list;
+      }).then(function(list){
+        _.forEach(list, function(item){
+          //TODO: Cache this info on the server
+          that.accountService.getByIdentityId(item.id).then(function(accountInfo){
+            item.activeCouponsCount = _.filter(accountInfo.coupons, 'isActive').length;
+          });
+        });
       });
     }
   };
