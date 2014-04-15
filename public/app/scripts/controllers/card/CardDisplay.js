133312e358ec03041a6038caeda11da59b813e94
wassertim <tim@digitalband.ru>
2014-04-15 21:18:10 +0400

    pin codes

--- public/app/scripts/controllers/card/CardDisplay.js
+++ public/app/scripts/controllers/card/CardDisplay.js
@@ -5,18 +5,28 @@
       var that = this;
       this.$state = $state;
       this.cardService = cardService;
+      this.card = {};
       cardService.get($state.params.number, $state.params.userId).then(function(card){
         that.card = card;
+        that.barcodeUrl = that.getBarcodeUrl(that.card);
       });
     }
-
     CardDisplayController.prototype = {
       savePin: function(){
         var that = this;
         that.isSaving = true;
         this.cardService.savePin(this.card.pinCode, this.card.number, this.$state.params.userId).then(function(){
           that.isSaving = false;
+          that.barcodeUrl = that.getBarcodeUrl(that.card);
         });
+      },
+      getBarcodeUrl: function(card) {
+        if (card.pinCode) {
+          var v = Math.random();
+          return jsRoutes.controllers.BarCode.cardBarCode(this.$state.params.number, this.$state.params.userId).url + "?v=" + v;
+        } else {
+          return "";
+        }
       }
     };
     app.controller('CardDisplayController', CardDisplayController);
