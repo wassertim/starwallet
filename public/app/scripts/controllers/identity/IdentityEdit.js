23a9c72c1e7dd40a5fa4d1d207bb87aadddc327f
wassertim <tim@digitalband.ru>
2014-04-16 12:48:01 +0400

    removing identity

--- public/app/scripts/controllers/identity/IdentityEdit.js
+++ public/app/scripts/controllers/identity/IdentityEdit.js
@@ -16,36 +16,36 @@
 
   IdentityEditController.prototype = {
 
-    getForm: function(){
+    getForm: function () {
       var that = this;
       if (this.params['accountId']) {
         that.isLoading = true;
-        this.identityService.get(this.params['accountId'], this.params['userId']).then(function(accountInfo){
+        this.identityService.get(this.params['accountId'], this.params['userId']).then(function (accountInfo) {
           that.account = accountInfo;
           that.isLoading = false;
         })
       }
     },
 
-    save: function(account){
+    save: function (account) {
       var that = this;
       that.alert = undefined;
       that.isUpdating = true;
       if (!account.id) {
         account.id = 0;
-        this.identityService.add(account, this.params['userId']).then(function(accountId){
+        this.identityService.add(account, this.params['userId']).then(function (accountId) {
           that.isUpdating = false;
           that.$state.go('withNav.accountList.editAccount', angular.extend(that.params, {accountId: accountId}))
-        }, function(){
+        }, function () {
           that.isUpdating = false;
           that.alert = {
             message: 'Authentication error'
           };
         });
       } else {
-        this.identityService.update(account, this.params['userId']).then(function(){
+        this.identityService.update(account, this.params['userId']).then(function () {
           that.isUpdating = false;
-        }, function(){
+        }, function () {
           that.isUpdating = false;
           that.alert = {
             message: 'Authentication error'
@@ -54,7 +54,7 @@
       }
     },
 
-    remove: function(accountId){
+    remove: function (accountId) {
       var that = this;
       that.isUpdating = true;
       if (this.$window.confirm("Do you really want to delete this account?")) {
