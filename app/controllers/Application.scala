5ac6502cc1a9fd2251ed35cfd0d42226b054a3b6
wassertim <tim@digitalband.ru>
2014-07-01 10:42:53 +0400

    prod

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,7 +13,7 @@ class Application @Inject()(val starbucks: Starbucks) extends Controller {
   def index = Action {
     //replace is only needed for staged play deployment
     val pathToApp = play.Play.application().path().getAbsolutePath.replace("target/universal/stage", "")
-    val file = if (!Play.isProd) {
+    val file = if (Play.isProd) {
       "public/dist/index.html"
     } else {
       "index.html"
